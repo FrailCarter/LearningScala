861f7277ba9984f2635cf6e8cead77bc657323fa
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-03 15:38:56 -0400

    Added comments surrounding problems with code

--- Cafe.scala
+++ Cafe.scala
@@ -1,9 +1,17 @@
 
+// Goal is to handle purchases at a coffee shop
+
+
 class Cafe {
 
     def buyCoffee(cc: CreditCard): Coffee = {
         val cup = new Coffee()
         cc.charge(cup.price)
+        // Side efffect is charging card
         cup 
+        // returns cup of coffee
     }
 }
+
+// If we are returning a cup of coffee, then why charge the credit card (let's say it also is contacting the outside world)
+// this introduces things that are hard to test.
