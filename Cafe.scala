eabd0870c22db46c1dfe11473505291adacaa524
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-03 15:43:10 -0400

    Added Payment interface. Added comments about the advantages and disadvantages of such an implementation

--- Cafe.scala
+++ Cafe.scala
@@ -4,10 +4,10 @@
 
 class Cafe {
 
-    def buyCoffee(cc: CreditCard): Coffee = {
+    def buyCoffee(cc: CreditCard , p: Payments): Coffee = {
         val cup = new Coffee()
-        cc.charge(cup.price)
-        // Side efffect is charging card
+        p.charge(cc , cup.price)
+        // Payments can be an interface now, so we can write a mock interface for testing
         cup 
         // returns cup of coffee
     }
@@ -15,3 +15,6 @@ class Cafe {
 
 // If we are returning a cup of coffee, then why charge the credit card (let's say it also is contacting the outside world)
 // this introduces things that are hard to test.
+
+// If we want to test that buyCoffee  creates a charge equal to the price of a cup of coffee, this becomes overkill. 
+// Additionally, we can't reuse this easily. If I want to buy 12 coffees, that's 12 separate charges.
