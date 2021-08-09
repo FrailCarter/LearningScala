3246d4eba5029e8734f38d84a5425313354bd9b6
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-09 14:33:36 -0400

    Adding notes about Tail Call Recursion

--- FunctionalLoops.scala
+++ FunctionalLoops.scala
@@ -17,3 +17,18 @@ def factorial(n: Int): Int = {
 
 // The way to write loops functionally w/o a loop variable is via recursion 
 // "go" in this cas is a recursive helper function
+
+/*
+Tail calls:
+    A call is in tail position if the caller does nothing other than return the value
+    of the recursive call.
+
+    If all recursive calls made by a function are in tail position, Scala automatilcally compiles
+    the recusion to iterative loops that don't consume call sack frames for
+    each iteration. 
+    
+    By default, Scala does not tell us if tail call elimination was successful, but if we 
+    are expecting this to occur, we can tell the compiler about this assumption by using
+    tailrec annotation, which will throw a compilation error if it's unable to eliminate the tail
+    calls.
+*/
