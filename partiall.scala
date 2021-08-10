10c221c5a52fd0825492d05e36b34f3e8460ddb6
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-10 15:21:44 -0400

    Adding first component to the body of partiall

--- partiall.scala
+++ partiall.scala
@@ -1,6 +1,10 @@
 
 
 def partiall[A , B , C](a: A , f: (A,B) => C): B => C 
+    (b: B) => // ???
+    // partiall has to return a function that takes an arg of type B, so we start there
+
+
 // Function takes 3 type parameters: A, B, C
 // Takes 2 args: a of type A, and f which is a function that takes 2 params
 // of types A and B, respectively, and returns a value of type C.
