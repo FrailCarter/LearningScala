366b74ab5cdb127fd169618de545a957b2144120
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-10 15:24:18 -0400

    Done!

--- partiall.scala
+++ partiall.scala
@@ -6,6 +6,8 @@ def partiall[A , B , C](a: A , f: (A,B) => C): B => C
     // We know that the type C has to come from the function, f. So to get C, we need to pass 
     // A and B to f
 
+    // Done!
+
 
 // Function takes 3 type parameters: A, B, C
 // Takes 2 args: a of type A, and f which is a function that takes 2 params
