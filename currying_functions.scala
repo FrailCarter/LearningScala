846e702682f464f2446b0130b07edeaf60f6f27f
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-11 15:30:59 -0400

    Yay things are starting to work

--- currying_functions.scala
+++ currying_functions.scala
@@ -23,12 +23,30 @@ def mult(x: Int , y: Int): Int = {
 Wonderful, except that mult and sum look almost identical, don't they?
 What if we could combine the two?
 */
-def app_func(x: Int , y: Int , f: (Int , Int) => Int): Int = {
+def appFunc(x: Int , y: Int , f: (Int , Int) => Int): Int = {
     f(x , y)
 }
 
 
-app_func(5 , 6 , sum)
-app_func(5 , 6 , mult)
+appFunc(5 , 6 , sum)
+appFunc(5 , 6 , mult)
 // Great, this works
-// What if I want to sum or mult more than 2 numbers? Hmmm...
+// What if I want to do string things with numbers?
+
+def makeStr(x: Int , y: Int): String = {
+    val msg = "The combination of %d and %d is %s"
+    val a = x.toString
+    val b = y.toString
+    msg.format(x , y , x + y)
+}
+makeStr(5 , 6)
+
+// This is still kind of the same deal as appFunc
+// What if appFunc could take a generic type?
+def appFunc[A , B](a: A , b: A , f: (A , A) => B): B = {
+    f(a , b)
+}
+
+appFunc(5 , 6 , makeStr)
+appFunc(5 , 6 , mult)
+appFunc(5 , 6 , sum)
