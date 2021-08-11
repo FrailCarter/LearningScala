b2ba5a9060d0f432fcbf03d000cacb340ce1232b
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-11 17:13:27 -0400

    Changed sum to add and extended the add and mult functions to be more verbose

--- currying_functions.scala
+++ currying_functions.scala
@@ -5,10 +5,10 @@ Currying functions (or partialling functions)
 */
 
 /*
-Lets make a function sums 2 ints
+Lets make a function adds 2 ints
 */
 
-def sum(x: Int , y: Int): Int  = {
+def add(x: Int , y: Int): Int  = {
     x + y
 }
 
@@ -20,7 +20,7 @@ def mult(x: Int , y: Int): Int = {
 }
 
 /*
-Wonderful, except that mult and sum look almost identical, don't they?
+Wonderful, except that mult and add look almost identical, don't they?
 What if we could combine the two?
 */
 def appFunc(x: Int , y: Int , f: (Int , Int) => Int): Int = {
@@ -28,7 +28,7 @@ def appFunc(x: Int , y: Int , f: (Int , Int) => Int): Int = {
 }
 
 
-appFunc(5 , 6 , sum)
+appFunc(5 , 6 , add)
 appFunc(5 , 6 , mult)
 // Great, this works
 // What if I want to do string things with numbers?
@@ -49,11 +49,11 @@ def appFunc[A , B](a: A , b: A , f: (A , A) => B): B = {
 
 appFunc(5 , 6 , makeStr)
 appFunc(5 , 6 , mult)
-appFunc(5 , 6 , sum)
+appFunc(5 , 6 , add)
 
-// Excellent, now what if sum, mult, and makeStr took any number of args?
+// Excellent, now what if add, mult, and makeStr took any number of args?
 
-def sum(x: Array[Int]): Int = {
+def add(x: Array[Int]): Int = {
     x.reduce((y, z) => y + z)
 }
 
@@ -61,6 +61,35 @@ def mult(x: Array[Int]): Int = {
     x.reduce((y , z) => y * z)
 }
 
-sum(Array[Int](1 , 2 , 4))
+add(Array[Int](1 , 2 , 4))
 mult(Array[Int](1 , 2 , 4))
-// Okay, that's better
+// Okay, that's better.
+
+def add(x: Array[Int]): Int = {
+    def go(n: Int , total: Int): Int = {
+        if (n == x.length) {
+            total 
+        }
+        else {
+            go(n + 1 , total + x(n))
+        }
+    }
+    go(0 , 0)
+}
+
+def mult(x: Array[Int]): Int = {
+    def go(n: Int , total: Int): Int = {
+        if (n == x.length) {
+            total 
+        }
+        else {
+            go(n + 1 , total * x(n))
+        }
+    }
+    go(0 , 1)
+}
+
+add(Array[Int](1 , 2 , 4))
+mult(Array[Int](1 , 2 , 4))
+
+// Even better, now these are *really* starting to look similar
