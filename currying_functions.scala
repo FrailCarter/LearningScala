8191b5b0cebcb3b6ab91b73381e837825bb088d2
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-11 15:15:06 -0400

    Practicing more function making

--- currying_functions.scala
+++ currying_functions.scala
@@ -3,16 +3,32 @@
 /*
 Currying functions (or partialling functions)
 */
-def sum(x: Int , y: Int): Int = {
-    x + y 
-}
-sum(1 , 2)
-// This is not curried 
-// Currying involves taking a fxn with multiple args and turning them into fxn calls with 
-// one arg
 
-def curriedSum(x: Int): Int => Int = {
-    (y: Int) => x + y
+/*
+Lets make a function sums 2 ints
+*/
+
+def sum(x: Int , y: Int): Int  = {
+    x + y
 }
-curriedSum(1)(2) 
-// returns 3! Yay!
+
+/*
+Great, however, what if I want to multiply 2 ints?
+*/
+def mult(x: Int , y: Int): Int = {
+    x * y 
+}
+
+/*
+Wonderful, except that mult and sum look almost identical, don't they?
+What if we could combine the two?
+*/
+def app_func(x: Int , y: Int , f: (Int , Int) => Int): Int = {
+    f(x , y)
+}
+
+
+app_func(5 , 6 , sum)
+app_func(5 , 6 , mult)
+// Great, this works
+// What if I want to sum or mult more than 2 numbers? Hmmm...
