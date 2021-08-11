0c067a39bb3d40137d7295ee01b1a3d845d901d7
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-11 17:13:45 -0400

    Added braces

--- partiall.scala
+++ partiall.scala
@@ -1,13 +1,13 @@
 
 
-def partiall[A , B , C](a: A , f: (A,B) => C): B => C 
+def partiall[A , B , C](a: A , f: (A,B) => C): B => C = {
     (b: B) => f(a , b)
     // partiall has to return a function that takes an arg of type B, so we start there
     // We know that the type C has to come from the function, f. So to get C, we need to pass 
     // A and B to f
 
     // Done!
-
+}
 
 // Function takes 3 type parameters: A, B, C
 // Takes 2 args: a of type A, and f which is a function that takes 2 params
