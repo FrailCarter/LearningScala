4a997becf25b7b6b72ba3ad8bc0e3dbc8373c29c
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-26 11:31:16 -0400

    Added example of sum for currying fxns

--- curry.scala
+++ curry.scala
@@ -4,11 +4,16 @@
 2 args into a function of one arg that partially applies f
 */
 
-def curry[A , B , C](f: (A , B) => C): A => B => C = 
+def curry[A , B , C](f: (A , B) => C): A => B => C = {
     // This function ret a function that takes an arg of A and returns B, then takes B and returns C
     // So we need to start with A, since f takes A and B as params to return C
     (a: A) => (b: B) => f(a , b)
-    // Start with A, then move to B, then send those to f(a ,b)
+    // Start with A, then apply A to B, then apply those to f(a ,b)
     
+}
 
+def sum(x: Int , y: Int): Int = {
+    x + y
+}
 
+curry(sum)(5)
