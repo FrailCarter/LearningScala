b306cb6c212901d0b6c788e2a4961647f4f99868
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-27 13:49:13 -0400

    Done. Added cases for if it's Nil, if we hit the EOL, and if we are somewhere in the middle

--- Chapter3/Exercise3_6.scala
+++ Chapter3/Exercise3_6.scala
@@ -49,4 +49,14 @@ object List {
         case Nil => a2 
         case Cons(h,t) => Cons(h , append(t , a2))
     }
+
+    def init[A](l: List[A]): List[A] = l match {
+        case Nil => Nil
+        case Cons(x , Nil) => Nil 
+        case Cons(x , y) => Cons(x , init(y))
+    }
+
 }
+val x: List[Int] = List(1,2,3,4,5)
+val y: Boolean = List.init(x) == List(1,2,3,4)
+
