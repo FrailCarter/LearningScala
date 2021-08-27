96d60ff888622c461adf99c7ff96d24a105e0673
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-27 10:38:01 -0400

    Implementing dropWhile

---
+++ Chapter3/Exercise3_5.scala
@@ -0,0 +1,7 @@
+
+
+// Implement dropWhile, which removes elements from the List prefix so long as they match a predicate 
+
+def dropWhile[A](l: List[A] , f: A => Boolean): List[A] = l match {
+    
+}
