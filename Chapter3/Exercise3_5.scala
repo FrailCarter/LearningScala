b59198ff681ae86025b2b0dc20795efcef5630d6
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-27 10:52:45 -0400

    Done!

--- Chapter3/Exercise3_5.scala
+++ Chapter3/Exercise3_5.scala
@@ -2,6 +2,54 @@
 
 // Implement dropWhile, which removes elements from the List prefix so long as they match a predicate 
 
-def dropWhile[A](l: List[A] , f: A => Boolean): List[A] = l match {
-    
+sealed trait List[+A]
+case object Nil extends List[Nothing] 
+case class Cons[+A](head: A , tail: List[A]) extends List[A] 
+
+object List {
+    def sum(ints: List[Int]): Int = ints match {
+        case Nil => 0
+        case Cons(x , xs) => x + sum(xs)
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+        case Nil => 1.0 
+        case Cons(0.0 , _) => 0.0 
+        case Cons(x , xs) => x * product(xs)
+    }
+
+    def apply[A](as: A*): List[A] = {
+        if (as.isEmpty) {
+            Nil 
+        }
+        else {
+            Cons(as.head , apply(as.tail: _*))
+        }
+    }
+
+    def tail[A](as: List[A]): List[A] = as match {
+        case Nil => Nil
+        case Cons(x , xs) => xs
+    }
+
+    def setHead[A](a: A , as: List[A]): List[A] = as match {
+        case Nil => Cons(a , Nil)
+        case Cons(x , xs) => Cons(a , xs)
+    }
+
+    def drop[A](l: List[A] , n: Int): List[A] = l match {
+        case Nil => Nil 
+        case Cons(x , xs) => if (n == 0) {Cons(x , xs)} else {drop(xs , n - 1)}
+    }
+
+    def dropWhile[A](l: List[A] , f: A => Boolean): List[A] = l match {
+        case Nil => Nil 
+        case Cons(x , xs) => if (f(x)) {dropWhile(xs , f)} else {Cons(x , dropWhile(xs , f))}
+    }
 }
+
+val f = (y: Int) => if (y == 6) {true} else {false}
+val x: List[Int] = List(1,2,3,4)
+val y: Boolean = List.dropWhile(x , (y: Int) => if (y == 2) {true} else {false}) == List(1 , 3 , 4)
+val a: Boolean = List.dropWhile(x , f) == x
+val b: Boolean = List.dropWhile(List() , f) == Nil
