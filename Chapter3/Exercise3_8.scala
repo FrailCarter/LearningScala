60216aa97c8c7e138c99559b559d214ef02d9883
Carter Nivens <carternivens@Carters-MBP.attlocal.net>
2021-08-29 16:58:10 -0400

    Added comments for exercise

--- Chapter3/Exercise3_8.scala
+++ Chapter3/Exercise3_8.scala
@@ -66,5 +66,14 @@ object List {
 
 
 
-val x: List[Int] = List.foldRight(List(1,2,3) , Nil: List[Int])(Cons(_ , _))
+val x: List[Int] = List.foldRight(List(1,2,3,4,5,6) , Nil: List[Int])(Cons(_ , _))
+// Returns x: List[Int] = Cons(1,Cons(2,Cons(3,Cons(4,Cons(5,Cons(6,Nil))))))
+
+/*
+foldRight takes 2 params: as of type List[A] and z of type B
+The type of A determines the type of A in the second param list, and the type of B determines the type of B 
+in the second param list
+So when using the constructor for a list as f, then the return type has to be Cons (or List[A])
+So foldRight can be used to construct a list, I suppose
+*/
 
